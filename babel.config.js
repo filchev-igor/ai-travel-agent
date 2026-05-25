f98bb606bb07baf1ead93ef1a6c9d336b9eb6e5b
Filchev Igor <filchev.igor@gmail.com>
2026-05-25 05:07:31 +0300

    gemini

--- babel.config.js
+++ babel.config.js
@@ -1,30 +1,32 @@
 module.exports = function (api) {
-    api.cache(true);
-    return {
-        presets: ['babel-preset-expo'],
-        plugins: [
-            [
-                'module:react-native-dotenv',
-                {
-                    moduleName: '@env',
-                    path: '.env',
-                },
-            ],
-            [
-                'module-resolver',
-                {
-                    root: ['./'],
-                    alias: {
-                        '@': './src',
-                        '@components': './src/components',
-                        '@screens': './src/screens',
-                        '@services': './src/services',
-                        '@types': './src/types',
-                        '@assets': './assets',
-                    },
-                    extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
-                },
-            ],
-        ],
-    };
+  api.cache(true);
+  return {
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module:react-native-dotenv",
+        {
+          moduleName: "@env",
+          path: ".env",
+          safe: false,
+          allowUndefined: true,
+        },
+      ],
+      [
+        "module-resolver",
+        {
+          root: ["./"],
+          alias: {
+            "@": "./src",
+            "@components": "./src/components",
+            "@screens": "./src/screens",
+            "@services": "./src/services",
+            "@types": "./src/types",
+            "@assets": "./assets",
+          },
+          extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
+        },
+      ],
+    ],
+  };
 };
